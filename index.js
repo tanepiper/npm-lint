2b4c323ecd06a69de3ea3bfbdf785ad8bea290c9
Tane Piper <tane@backbase.com>
2017-07-12 11:14:46 +0200

    Refactor in dependency check

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ console.log(`Running package-linter`.green);
 const { promisify } = require('util');
 const fs = require('fs');
 const path = require('path');
+const Table = require('cli-table');
 const cwd = process.cwd();
 
 const readFileAsync = promisify(fs.readFile);
@@ -15,6 +16,7 @@ const readDirAsync = promisify(fs.readdir);
 
 const checkFiles = require(`${__dirname}/lib/check-files`);
 const loadRule = require(`${__dirname}/lib/load-rule`);
+const loadScan = require(`${__dirname}/lib/load-scan`);
 
 // Check special rules first
 let errors = [];
@@ -33,104 +35,44 @@ checkFiles(cwd)
         });
 
         const results = Promise.all(rulesReports);
-        
-        results.then((results)=> {
-            results.forEach(result => {
-                console.log(`${result.name}`.underline);
-                if (!result.errors || result.errors && result.errors.length === 0) {
-                    return console.log(`No Errors`.green);
-                }
-                result.errors.forEach(error => {
-                    console.error(`${error.message}`.red.bold);
-                })
 
+        results
+            .then(results => {
+                results.forEach(result => {
+                    console.log(`${result.name}`.underline);
+                    if (!result.errors || (result.errors && result.errors.length === 0)) {
+                        return console.log(`No Errors`.green);
+                    }
+                    result.errors.forEach(error => {
+                        console.error(`${error.message}`.red.bold);
+                    });
+                });
+                return Promise.resolve(null);
             })
-        });
+            .then(() => {
 
-        // const errorsGroups = rulesToRun.map(rule => {
-        //     return rule.module.processor(context, context.projectRules[rule.key]);
-        // }).filter(error => error);
+                if (context.projectRules.dependencies.checkLatest) {
+                    const scanner = loadScan('dependency_version_check', __dirname);
 
-        // Promise.all(errorsGroups).then(() => {
-        //     console.log(arguments);
+                    const result = scanner.processor(context);
+                    result.then(results => {
+                        results.result.then(upgrades => {
+                            const table = new Table({
+                                head: ['Package', 'Package Version', 'Latest Version'],
+                                colWidths: [40, 30, 30]
+                            });
 
-        // //     .then(resolved => {
-        // //     resolved.forEach(display => {
-        // //         if (display.name === 'latest') {
-        // //             const table = new Table({
-        // //                 head: ['Package', 'Package Version', 'Latest Version'],
-        // //                 colWidths: [40, 30, 30]
-        // //             });
-
-        // //             Object.keys(display.upgrades).forEach(dep => {
-        // //                 table.push([dep, context.package.dependencies[dep], display.upgrades[dep]]);
-        // //             });
-        // //             console.log(table.toString());
-        // //         }
-        // //     });
-        // // });
-        // });
-
-        // errorsGroups.forEach(errorGroup => {
-        //     if (errorGroup.errors) {
-        //         errorGroup.errors.forEach(error => {
-
-        //             // If we have an error, and the exit code isn't set, then set it to 1
-        //             if (error.level === 'error' && !exitCode) {
-        //                 exitCode = 1;
-        //             }
-
-        //             const colours = {
-        //                 'error': 'red'
-        //             }
-
-        //             console[error.level](error.message[colours[error.level]]);
-        //         });
-        //     }
-        // });
-
-        // if (!exitCode) {
-        //     console.log(`Your NPM file is all good`.bold.yellow + `\x20\u2713`.green);
-        // }
-        // process.exit(exitCode);
+                            Object.keys(upgrades.upgrades).forEach(upgrade => {
+                                table.push([upgrade, context.package.dependencies[upgrade], upgrades.upgrades[upgrade]]);
+                            });
+                            console.log(`Available Dependency Updates`.underline.cyan);
+                            console.log(table.toString());
+                        });
+                    });
+                }
+            });
     })
     .catch(error => {
         console.log(error);
         process.exit(1);
     });
-
-// fs.readdir(`${__dirname}/rules`, (err, ruleFiles) => {
-//     Object.keys(defaultRules).forEach(rule => {
-//         if (ruleFiles.includes(`${rule}.js`)) {
-//             const ruleFileIndex = ruleFiles.indexOf(`${rule}.js`);
-//             try {
-//                 const script = require(`${__dirname}/rules/${ruleFiles[
-//                     ruleFileIndex
-//                 ]}`);
-//                 rules[script.key] = script;
-//             } catch (e) {
-//                 console.log(`Unable to load rule ${rule}`);
-//             }
-//         }
-//     });
-
-//     Object.keys(rules).forEach(ruleKey => {
-//         console.log(`Running rule ${ruleKey}`);
-//         const rule = rules[ruleKey];
-//         const ruleErrors = rule.processor(package, defaultRules[ruleKey]);
-//         errors = errors.concat(ruleErrors);
-//     });
-
-//     let exitCode = 0;
-
-//     if (errors.length > 0) {
-//         exitCode = 1;
-//         errors.forEach(error => {
-//             console.error(`Rule: ${error}`);
-//         });
-//     } else {
-//         console.log('Your NPM file is all good');
-//     }
-
-//     process.exit(exitCode);
-// });
--- scans/dependency_version_check.js
+++ scans/dependency_version_check.js
@@ -1,11 +1,29 @@
+const ncu = require('npm-check-updates');
+
 module.exports = {
     name: 'Dependency Version Check',
     key: 'dependency_version_check',
-    processor: (context) => {
-        
+    processor: context => {
+        return Promise.resolve({
+            name: module.exports.name,
+            key: module.exports.key,
+            result: ncu
+                .run({
+                    packageData: JSON.stringify(context.package),
+                    args: Object.keys(context.package.dependencies),
+                    silent: false,
+                    jsonUpgraded: true
+                })
+                .then(upgrades => {
+                    return {
+                        upgrades,
+                        totalUpgrades: (upgrades && Object.keys(upgrades).length) || 0
+                    };
+                })
+            
+        });
     }
-}
-
+};
 
 //  if (rules.checkLatest) {
 //                 try {
