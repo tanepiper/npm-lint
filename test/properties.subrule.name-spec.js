2560af8a03e369e9c063478c5e74c9a177c82ab2
Tane Piper <tane@backbase.com>
2017-07-13 15:01:41 +0200

    Fix tests

--- test/properties.subrule.name-spec.js
+++ test/properties.subrule.name-spec.js
@@ -1,70 +1,106 @@
 const test = require('ava');
+const sinon = require('sinon');
 
 const propertiesName = require('../rules/subrules/properties-name');
 
 test('Check that name is less than 214 characters', t => {
-    t.plan(1);
-
-    const package1 = {
-        name:
-            'R062f9B5fgN7GYk83qpZt4qVO4CwZKPgsNQE973fQIHjvYp9rbn8BzfTPKB8PyVmv7Vt4lP8YDlJY2XbVC442QG7mC1tnCB9VQEiJBXP30UbBy16jWeQkANE209g8W9rBBkGz4A5JI89aTzDLla2cF5xyXSkiBA0wcVV5HAucstPfNLuCZJSA1xKUXBJvtHF8uD8sl8igeYSVPBLVs6hU2n'
-    };
-
-    t.is(propertiesName.processor(package1.name).message, 'package.json "name" property cannot be longer than 214 characters');
-});
-
-test('Check that name does not begin with . or _', t => {
     t.plan(2);
 
-    const package1 = {
-        name: '.my-package'
+    const context = {
+        package: {
+            name:
+                'R062f9B5fgN7GYk83qpZt4qVO4CwZKPgsNQE973fQIHjvYp9rbn8BzfTPKB8PyVmv7Vt4lP8YDlJY2XbVC442QG7mC1tnCB9VQEiJBXP30UbBy16jWeQkANE209g8W9rBBkGz4A5JI89aTzDLla2cF5xyXSkiBA0wcVV5HAucstPfNLuCZJSA1xKUXBJvtHF8uD8sl8igeYSVPBLVs6hU2n'
+        },
+        errors: {
+            insert: () => {}
+        }
     };
 
-    t.is(propertiesName.processor(package1.name).message, 'package.json "name" property cannot start with a . (dot) or _ (underscore)');
+    const spy = sinon.spy(context.errors, 'insert');
+    propertiesName.processor(context)
 
-    const package2 = {
-        name: '_my-package'
+    t.true(spy.called);
+    t.true(spy.calledWith({message: 'package.json "name" property cannot be longer than 214 characters'}));
+});
+
+test('Check that name does not begin with .', t => {
+    t.plan(2);
+
+    const context = {
+        package: {
+            name:
+                '.my-package'
+        },
+        errors: {
+            insert: () => {}
+        }
     };
+    const spy = sinon.spy(context.errors, 'insert');
 
-    t.is(propertiesName.processor(package2.name).message, 'package.json "name" property cannot start with a . (dot) or _ (underscore)');
+    propertiesName.processor(context)
+    t.true(spy.calledWith({message: 'package.json "name" property cannot start with a . (dot) or _ (underscore)'}));
+
+    context.package.name = '_my-package';
+    propertiesName.processor(context)
+    t.true(spy.calledWith({message: 'package.json "name" property cannot start with a . (dot) or _ (underscore)'}));
 });
 
 test('Check that name does not being with a capital letter', t => {
     t.plan(1);
 
-    const package1 = {
-        name: 'My-package'
+    const context = {
+        package: {
+            name:
+                'My-package'
+        },
+        errors: {
+            insert: () => {}
+        }
     };
-
-    t.is(propertiesName.processor(package1.name).message, 'package.json "name" property cannot start with a capital letter');
+    const spy = sinon.spy(context.errors, 'insert');
+    propertiesName.processor(context);
+    t.true(spy.calledWith({message: 'package.json "name" property cannot start with a capital letter'}));
 });
 
 test('Check that name does not contain non-URL-safe characters', t => {
     t.plan(3);
 
-    const package1 = {
-        name: '/My_Package'
+    const context = {
+        package: {
+            name:
+                '/my-package'
+        },
+        errors: {
+            insert: () => {}
+        }
     };
-    t.is(propertiesName.processor(package1.name).message, 'package.json "name" property cannot contain non-URL-safe characters');
+    const spy = sinon.spy(context.errors, 'insert');
+    propertiesName.processor(context);
+    t.true(spy.calledWith({message: 'package.json "name" property cannot contain non-URL-safe characters'}));
 
-    const package2 = {
-        name: 'my_p?ckage'
-    };
-    t.is(propertiesName.processor(package2.name).message, 'package.json "name" property cannot contain non-URL-safe characters');
+    context.package.name = 'my_p?ckage';
+    propertiesName.processor(context);
+    t.true(spy.calledWith({message: 'package.json "name" property cannot contain non-URL-safe characters'}));
 
-    const package3 = {
-        name: 'my_package^'
-    };
-    t.is(propertiesName.processor(package3.name).message, 'package.json "name" property cannot contain non-URL-safe characters');
+    context.package.name = 'my_package^';
+    propertiesName.processor(context);
+    t.true(spy.calledWith({message: 'package.json "name" property cannot contain non-URL-safe characters'}));
 });
 
 test('Check that name is passing when valid', t => {
     t.plan(1);
 
-    const package1 = {
-        name:
-            'valid-module_name'
+    const context = {
+        package: {
+            name:
+                'my-package'
+        },
+        errors: {
+            insert: () => {}
+        }
     };
+    const spy = sinon.spy(context.errors, 'insert');
 
-    t.is(propertiesName.processor(package1.name), undefined);
+    propertiesName.processor(context);
+    t.false(spy.called);
 });
--- test/properties.subrule.version-spec.js
+++ test/properties.subrule.version-spec.js
@@ -1,12 +1,55 @@
 const test = require('ava');
+const sinon = require('sinon');
 
 const propertiesVersion = require('../rules/subrules/properties-version');
 
-test('It should pass with valid semvers', t => {
-
+test('It should fail with invalid semvers', t => {
     t.plan(3);
 
-    t.is(propertiesVersion.processor('1.2.3'), undefined);
-    t.is(propertiesVersion.processor('v1.0.0'), undefined);
-    t.is(propertiesVersion.processor('1.2.3-alpha.10.beta.0+build.unicorn.rainbow'), undefined);
+    const context = {
+        package: {
+            version: 'latest'
+        },
+        errors: {
+            insert: () => {}
+        }
+    };
+    const spy = sinon.spy(context.errors, 'insert');
+
+    propertiesVersion.processor(context);
+    t.true(spy.calledWith({ message: `package.json "version" property is not a valid semver` }));
+
+    context.package.version = 'https://github.com/tanepiper/npm-lint';
+    propertiesVersion.processor(context);
+    t.true(spy.calledWith({ message: `package.json "version" property is not a valid semver` }));
+
+    context.package.version = '1,2.3';
+    propertiesVersion.processor(context);
+    t.true(spy.calledWith({ message: `package.json "version" property is not a valid semver` }));
+
+});
+
+test('It should pass with valid semvers', t => {
+    t.plan(3);
+
+    const context = {
+        package: {
+            version: '1.2.3'
+        },
+        errors: {
+            insert: () => {}
+        }
+    };
+    const spy = sinon.spy(context.errors, 'insert');
+
+    propertiesVersion.processor(context);
+    t.false(spy.called);
+
+    context.package.version = 'v1.0.1';
+    propertiesVersion.processor(context);
+    t.false(spy.called);
+
+    context.package.version = '1.2.3-alpha.10.beta.0+build.unicorn.rainbow';
+    propertiesVersion.processor(context);
+    t.false(spy.called);
 });
